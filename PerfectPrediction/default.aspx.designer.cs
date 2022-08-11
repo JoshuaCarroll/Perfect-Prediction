2906295c0e001ecd48a5d95d70e843ffaf1aabb2
Joshua Carroll <joshcarroll21@gmail.com>
2022-08-10 21:25:30 -0500

    Design enhancements

--- PerfectPrediction/default.aspx.designer.cs
+++ PerfectPrediction/default.aspx.designer.cs
@@ -123,6 +123,15 @@ namespace PerfectPrediction
         protected global::System.Web.UI.WebControls.TextBox txtName;
 
         /// <summary>
+        /// txtName_TextBoxWatermarkExtender control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.TextBoxWatermarkExtender txtName_TextBoxWatermarkExtender;
+
+        /// <summary>
         /// RequiredFieldValidator1 control.
         /// </summary>
         /// <remarks>
